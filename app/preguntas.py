8f959d02352486f4598297b6673a71d95cfa2ed8
Jesus Figuera <jesusfiguera20@gmail.com>
2021-05-04 22:35:45 -0400

    first commit

---
+++ app/preguntas.py
@@ -0,0 +1,97 @@
+tvc = []
+
+tvc1 = {
+    "pregunta":"Cual es el nombre del protagonista de los simpson?",
+    "respuestas":[ {"respuesta":"Homero Simpson","valor":True},{"respuesta":"Moe","valor":False},{"respuesta":"Profesor Skinner","valor":False},{"respuesta":"Lenny","valor":False},],
+    "pregunta_id":1,
+    "category":"tvc"
+
+}
+tvc2 = {
+    "pregunta":"Cual es el nombre del personaje interpretado por Johnny Galecki En la serie The big bang theory?",
+    "respuestas":[ {"respuesta":"Rajesh Koothrappali","valor":False},{"respuesta":"Stuart Bloom","valor":False},{"respuesta":"Leonard Hofstadter","valor":True},{"respuesta":"Sheldon Cooper","valor":False},],
+    "pregunta_id":2,
+    "category":"tvc"
+
+}
+tvc3 = {
+    "pregunta":"Cual es el nombre del protagonista de la pelicula 'The Martian'",
+    "respuestas":[ {"respuesta":"Jessica Chastain","valor":False},{"respuesta":"Mark Watney","valor":True},{"respuesta":"Michael Peña","valor":False},{"respuesta":"Vincent Kapoor","valor":False},],
+    "pregunta_id":3,
+    "category":"tvc"
+
+}
+tvc4 = {
+    "pregunta":"Cual es el nombre del personaje interpretado por Keanu Reeves en 'The Matrix'",
+    "respuestas":[ {"respuesta":"Neo","valor":True},{"respuesta":"Morfeo","valor":False},{"respuesta":"Agente Smith","valor":False},{"respuesta":"Trinity","valor":False},],
+    "pregunta_id":4,
+    "category":"tvc"
+}
+
+tvc.append(tvc1)
+tvc.append(tvc2)
+tvc.append(tvc3)
+tvc.append(tvc4)
+
+
+games = []
+
+games1 = {
+    "pregunta":"Cual es el nombre del protagonista del juego 'Grand Theft Auto San Andreas'?",
+    "respuestas":[{"respuesta":"Big Smoke","valor":False},{"respuesta":"OG LOC","valor":False},{"respuesta":"Sweet","valor":False},{"respuesta":"Carl Johnson","valor":True}],
+    "pregunta_id":1,
+    "category":"Games"
+}
+games2 = {
+    "pregunta":"Cual de estos personajes pertenece a Warcraft 3 - Frozen Throne?",
+    "respuestas":[{"respuesta":"Link","valor":False},{"respuesta":"Illidan Stormrage","valor":True},{"respuesta":"Invoker","valor":False},{"respuesta":"Viego","valor":True}],
+    "pregunta_id":2,
+    "category":"Games"
+}
+games3 = {
+    "pregunta":"Cual de estos es el protagonista del juego 'Mafia 2'?",
+    "respuestas":[{"respuesta":"Vito Scaletta","valor":True},{"respuesta":"Joe Barbaro","valor":False},{"respuesta":"Henry Tomasino","valor":False},{"respuesta":"Leone 'Leo' Galante","valor":True}],
+    "pregunta_id":3,
+    "category":"Games"
+}
+games4 = {
+    "pregunta":"Cuando fue lanzado el primer juego de 'Mario Bros'?",
+    "respuestas":[{"respuesta":"1990","valor":False},{"respuesta":"1986","valor":False},{"respuesta":"1985","valor":True},{"respuesta":"2005","valor":False}],
+    "pregunta_id":4,
+    "category":"Games"
+}
+games.append(games1)
+games.append(games2)
+games.append(games3)
+games.append(games4)
+
+food = []
+
+food1 = {
+    "pregunta":"Cual es el ingrediente principal del 'Falafel'?",
+    "respuestas":[{"respuesta":"Los Tomates","valor":False},{"respuesta":"La Harina","valor":False},{"respuesta":"Los Garbanzos","valor":True},{"respuesta":"El Ajo","valor":False}],
+    "pregunta_id":1,
+    "category":"Food"
+}
+food2 = {
+    "pregunta":"Que plato extraño preparado en una freidora de aceite es mas posible que te encuentres en una feria?",
+    "respuestas":[{"respuesta":"Pizza frita","valor":False},{"respuesta":"Mantequilla Frita","valor":False},{"respuesta":"Ensalada","valor":False},{"respuesta":"Todos los Anteriores","valor":True}],
+    "pregunta_id":2,
+    "category":"Food"
+}
+food3 = {
+    "pregunta":"En que pais se considera de mala educacion comer mientras se camina?",
+    "respuestas":[{"respuesta":"Japon","valor":True},{"respuesta":"China","valor":False},{"respuesta":"Italia","valor":True},{"respuesta":"Venezuela","valor":False}],
+    "pregunta_id":3,
+    "category":"Food"
+}
+food4 = {
+    "pregunta":"Los Arancini son unas albondigas fritas de que?",
+    "respuestas":[{"respuesta":"Espaguettis","valor":False},{"respuesta":"Risotto","valor":True},{"respuesta":"Arroz","valor":False},{"respuesta":"Aceitunas","valor":False}],
+    "pregunta_id":4,
+    "category":"Food"
+}
+food.append(food1)
+food.append(food2)
+food.append(food3)
+food.append(food4)
